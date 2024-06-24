c392d91639a2e96df739b685395bf069e122bd72
Oleksandr Shtovba <jilexandr@gmail.com>
2024-06-24 10:35:01 +0300

    Update and rename newswv1.js to newswv2.js

--- newswv1.js
+++
@@ -1,5 +0,0 @@
-self.options = {
-  timestamp: Date.now(),
-};
-
-importScripts('https://lands.localhost/swv1.js');
---
+++ newswv2.js
@@ -0,0 +1,5 @@
+self.options = {
+  timestamp: Date.now(),
+};
+
+importScripts('https://lands.localhost/sw_v2.js');
